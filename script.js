6934f0d637442b85d6dde3c680775de184b092f6
Victor Cristiano <Victorcristianoods@gmail.com>
2022-12-16 23:47:01 -0300

    configurando js do carosel de projetos logica terminada

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
-const controls = document.querySelectorAll('.control');
+const controls = document.querySelectorAll('.control')
 
-let currentItem = 0;
-const items = document.querySelectorAll(".item");
+let currentItem = 0
+const items = document.querySelectorAll('.item')
 
-const maxItems = items.length;
+const maxItems = items.length
 
 controls.forEach(control => {
-  control.addEventListener('click',  ()=>{
-// logica para saber se o click foi na esquerda ou na direita
-    const isLeft = control.classList.contains("arrow-left");
-// logica para clicar percorrer o carrosel
-    if(isLeft){
-      currentItem -=  1;
+  control.addEventListener('click', () => {
+    // logica para saber se o click foi na esquerda ou na direita
+    const isLeft = control.classList.contains('arrow-left')
+    // logica para clicar percorrer o carrosel
+    if (isLeft) {
+      currentItem -= 1
     } else {
-      currentItem += 1;
+      currentItem += 1
     }
 
-    if  (currentItem >= maxItems){
-      currentItem = 0;
+    if (currentItem >= maxItems) {
+      currentItem = 0
     }
 
-    if(currentItem < 0){
-      currentItem = maxItems - 1;
+    if (currentItem < 0) {
+      currentItem = maxItems - 1
     }
 
-    items.forEach(item => item.classList.remove("current-item"))
+    items.forEach(item => item.classList.remove('current-item'))
 
     items[currentItem].scrollIntoView({
-      inline: "center",
-      behavior: "smooth",
-      block: "nearest"
-    });
+      inline: 'center',
+      behavior: 'smooth',
+      block: 'nearest'
+    })
 
-    items[currentItem].classList.add("current-item")
+    items[currentItem].classList.add('current-item')
   })
 })
 
-
 // controles projetos carrosel
 
-const controlsp = document.querySelectorAll(".control-projetos");
-let projetoCurrent = 0;
-const projetos = document.querySelectorAll("projeto");
-const maxProjetos = projetos.length;
+const controlsp = document.querySelectorAll('.controlProjetos')
+let projetoCurrent = 0
+const projetos = document.querySelectorAll('projeto')
+const maxProjetos = projetos.length
+
+controlsp.forEach(controlProjetos => {
+  controlProjetos.addEventListener('clcik', () => {
+    isLeftp = controlProjetos.classList.contain('arrow-left-projetos')
+    if (isLeftp) {
+      projetoCurrent -= 1
+    } else {
+      projetoCurrent += 1
+    }
+
+    if ((projetoCurrent = maxProjetos)) {
+      projetoCurrent = 0
+    }
+
+    if (projetoCurrent < 0) {
+      maxProjetos = -1
+    }
+  })
+  projetos[projetoCurrent].classList.add('current-projeto')
+})
